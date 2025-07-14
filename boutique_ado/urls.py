0e5230b72a2ca105c701146d179e800193e56a78
Obasohan2 <jtp7d03u@students.codeinstitute.net>
2025-07-14 18:19:07 +0100

    Added products views and templates

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),  # Include allauth URLs for authentication
     path('', include('home.urls')),
+    path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Serve media files during development
